da29aef718d48e4c42842c987c674e1dfd415511
Ryan <ryanlprince@aol.com>
2018-04-06 08:51:09 -0500

    completed scipt 4

---
+++ Script4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+e
+echo "input your number"
+read num
+count=2
+while [ $count -lt $num ]
+do 
+	if [ $((num%count)) -eq 0 ]
+	then 
+		echo is not prime
+		exit
+	else	
+		((++count))
+		
+	fi
+done
+echo is prime
